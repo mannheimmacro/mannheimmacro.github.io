fa440b330b8c1595ed7673ee6612324dfbcd2622
Krzysztof <k.pytka@gmail.com>
2018-05-07 17:12:46 +0200

    sheet

---
+++ assets/group1.R
@@ -0,0 +1,33 @@
+####################################################
+# Name and Family Name:
+# Student ID:
+# Date:  5/8/2018
+# Group: Rational preferences
+####################################################
+#  ___________
+# < Good luck!>
+#  -----------
+#   \     /\  ___  /\
+#    \   // \/   \/ \\
+#      ((    O O     ))
+#       \\ /     \ //
+#        \/  | |  \/
+#         |  | |  |
+#         |  | |  |
+#         |   o   |
+#         | |   | |
+#         |m|   |m|
+  
+
+
+#######Question 1
+##############Provide your answers below:
+
+
+
+
+#######Question 6
+##############Use the following code to import the data:
+data<-read.csv("https://raw.githubusercontent.com/mannheimmacro/mannheimmacro.github.io/master/assets/exam.csv")
+data<-data[,-1]
+##############Provide your answers below:
