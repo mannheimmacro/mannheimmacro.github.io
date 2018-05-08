a0981398dab579a5141c44db1148118e450a5249
Krzysztof <k.pytka@gmail.com>
2018-05-08 09:57:38 +0200

    final_exam

---
+++ assets/group2.R
@@ -0,0 +1,35 @@
+####################################################
+# Name and Family Name:
+# Student ID:
+# Date:  5/8/2018
+# Group: Positive orthant
+####################################################
+#  ___________
+# < Good luck!>
+#  -----------
+#   \     /\  ___  /\
+#    \   // \/   \/ \\
+#      ((    O O     ))
+#       \\ /     \ //
+#        \/  | |  \/
+#         |  | |  |
+#         |  | |  |
+#         |   o   |
+#         | |   | |
+#         |m|   |m|
+
+
+
+#######Question 1
+##############Provide your answers below:
+
+
+
+
+#######Question 6
+##############Use the following code to import the data:
+data<-read.csv("https://raw.githubusercontent.com/mannheimmacro/mannheimmacro.github.io/master/assets/exam2.csv")
+data<-data[,-1]
+##############Provide your answers below:
+
+
