bca233788445c302b7493cc94aa796f472ffb760
Krzysztof <k.pytka@gmail.com>
2019-05-13 11:05:32 +0200

    answers 2019

---
+++ assets/answers_2019.R
@@ -0,0 +1,38 @@
+####################################################
+# Name and Family Name:
+# Student ID:
+# Date:  5/13/2019
+# Group: Rational preferences
+####################################################
+#  ___________
+# < Good luck!>
+#  -----------
+#   \     /\  ___  /\
+#    \   // \/   \/ \\
+#      ((    O O     ))
+#       \\ /     \ //
+#        \/  | |  \/
+#         |  | |  |
+#         |  | |  |
+#         |   o   |
+#         | |   | |
+#         |m|   |m|
+
+
+
+#######Question 1
+##############Provide your answers below:
+
+
+#######Question 2
+##############Provide your answers below:
+
+
+
+#######Question 7
+##############Use the following code to import the data:
+data<-read.csv("https://raw.githubusercontent.com/mannheimmacro/mannheimmacro.github.io/master/assets/exam_2019.csv")
+data<-data[,-1]
+##############Provide your answers below:
+
+
